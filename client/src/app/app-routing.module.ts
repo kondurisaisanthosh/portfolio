6c86a1c0c159a2d61d3a31638ea188d5f2c79c83
kondurisaisanthosh <konduri.santosh96@gmail.com>
2020-03-17 04:17:21 -0400

    new

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,8 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './about/about/about.component';
+import { EducationComponent } from './education/education/education.component';
+import { ExperienceComponent } from './experience/experience/experience.component';
+import { SkillsComponent } from './skills/skills/skills.component';
+import { ProjectsComponent } from './projects/projects/projects.component';
+import { PersonalComponent } from './personal/personal/personal.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'about',component:AboutComponent},
+  {path:'education',component:EducationComponent},
+  {path:'experience',component:ExperienceComponent},
+  {path:'skills',component:SkillsComponent},
+  {path:'projects',component:ProjectsComponent},
+  {path:'personal-info',component:PersonalComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,10 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about/about.component';
+import { EducationComponent } from './education/education/education.component';
+import { ExperienceComponent } from './experience/experience/experience.component';
+import { SkillsComponent } from './skills/skills/skills.component';
+import { ProjectsComponent } from './projects/projects/projects.component';
+import { PersonalComponent } from './personal/personal/personal.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    EducationComponent,
+    ExperienceComponent,
+    SkillsComponent,
+    ProjectsComponent,
+    PersonalComponent,
+    
   ],
   imports: [
     BrowserModule,
